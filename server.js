d906c237acd733203f8e3a07f968b82b299a3760
Jeff Deskins <jeff.deskins@vanderbilt.edu>
2014-08-30 22:16:09 -0500

    Trying another socket.io sample

--- server.js
+++ server.js
@@ -6,14 +6,6 @@ var io = require('socket.io')(http);
 app.configure(function() {
 	app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);
   	app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
-	app.use(express.bodyParser());
-	app.use(express.methodOverride());
-	app.use(express.static(__dirname + '/public'));
-	app.use('/components', express.static(__dirname + '/components'));
-	app.use('/js', express.static(__dirname + '/js'));
-	app.use('/icons', express.static(__dirname + '/icons'));
-	app.set('views', __dirname + '/views');
-	app.engine('html', require('ejs').renderFile);
 });
 
 app.get('/', function(req, res){
