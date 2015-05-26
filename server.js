aca8c18746575cc7650bea92de08dfef069f7198
Jeff Deskins <Jeff.Deskins@lifeway.com>
2015-05-25 22:14:03 -0500

    update server config

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
 
 app.configure(function() {
 	app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);
@@ -84,7 +87,10 @@ io.on('connection', function(socket){
 });
 
 
-http.listen(app.get('port'), app.get('ipaddr'), function(){
-	console.log('Express server listening on  IP: ' + app.get('ipaddr') + ' and port ' + app.get('port'));
-});
+//http.listen(app.get('port'), app.get('ipaddr'), function(){
+//	console.log('Express server listening on  IP: ' + app.get('ipaddr') + ' and port ' + app.get('port'));
+//});
 
+http.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + port )
+});
